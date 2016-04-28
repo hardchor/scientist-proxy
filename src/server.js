0644fccecc1e837ce54346085989fbebc339d339
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2016-04-28 17:48:27 +0100

    working implementation of recording passthrough stream

--- src/server.js
+++ src/server.js
@@ -3,46 +3,28 @@ import http from 'http';
 import request from 'request';
 import config from '../config';
 import debug from 'debug';
-import RightStream from './RightStream';
+import RecordingTransformer from './stream/RecordingTransformer';
 
 const log = debug('app');
 const { oldApiEndpoint, newApiEndpoint } = config;
 
 const server = http.createServer((req, res) => {
-  const right = new RightStream();
-  // const left = new LeftStream();
+  const control = new RecordingTransformer('control');
+  const candidate = new RecordingTransformer('candidate');
 
-  //   // TODO: add X-Forwarded-For headers etc.
-  //   // TODO: forward to old api
-  //   // TODO: return old response
-  //   // TODO: forward to new API
-  // left
+  // TODO: add X-Forwarded-For headers etc.
+  // control
   req
     .pipe(request(oldApiEndpoint + req.url))
-    .on('response', (response) => {
-      // response is a https://nodejs.org/api/http.html#http_class_http_incomingmessage
-      log(`Status code: ${response.statusCode}`);
-      log('Headers:', response.headers);
-
-      response.on('data', (chunk) => {
-        log('Data', chunk.toString());
-      });
-      response.on('end', () => {
-        log('There will be no more data.');
-      });
-    })
-    .on('error', (err) => {
-      log(err);
-    })
-    // .pipe(right)
+    .pipe(control)
     .pipe(res)
   ;
 
-  // right
-  // req
-  //   .pipe(request(newApiEndpoint + req.url))
-  //   .pipe(right)
-  // ;
+  // candidate
+  req
+    .pipe(request(newApiEndpoint + req.url))
+    .pipe(candidate)
+  ;
 });
 
 const port = process.env.PORT || 3000;
